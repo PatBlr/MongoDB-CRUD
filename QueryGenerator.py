ec003963fe4c44b5d6431568d5f8140bf0501859
PatBlr <patrick-bleeker@t-online.de>
2022-01-25 21:43:36 +0100

    updated documentary

--- QueryGenerator.py
+++ QueryGenerator.py
@@ -6,7 +6,7 @@ import DBExceptions
 
 class QueryGenerator:
 
-    def __init__(self, statements: dict):
+    def __init__(self, statements: dict) -> None:
         """
         Resolves a dict of statements to a query, usable by the mongosh, a normal string representation of this query
         and a prettified (indented) version of this string\n
@@ -36,21 +36,33 @@ class QueryGenerator:
                 "contains": {"name": "$regex", "argument": "/{argument}/", "options": ""}
             }
         self.statements = statements
-        self.query = ""
+        self.query = {}
         self.query_string = ""
         self.query_string_pretty = ""
         self.__generate()
 
-    def get_query(self):
+    def get_query(self) -> dict:
+        """
+        Returns the generated query
+        :return: generated query as dict compatible with pymongo
+        """
         return self.query
 
-    def get_query_string(self):
+    def get_query_string(self) -> str:
+        """
+        Returns the generated string representation of the query
+        :return: generated query as string
+        """
         return self.query_string
 
-    def get_query_string_pretty(self):
+    def get_query_string_pretty(self) -> str:
+        """
+        Returns the generated and indented representation of the query
+        :return: generated query as indented string
+        """
         return self.query_string_pretty
 
-    def __generate(self):
+    def __generate(self) -> None:
         if len(self.statements) > 1:
             ands = defaultdict(dict)  # dict of dict
             ors = defaultdict(dict)  # dict
@@ -76,24 +88,21 @@ class QueryGenerator:
             query_string = self.__resolve_statement(self.statements["statement1"])
         self.__add_returns(query_string)
 
-    def __link_ands(self, ands, i=1, j=1):
+    def __link_ands(self, ands: dict, i: int = 1, j: int = 1) -> None:
         # and is stronger than or -> bind the ands and add them to a dict
-        try:
-            if i < len(self.statements):
-                if self.statements[f"statement{i+1}"]["clause"] == "and":
-                    if self.statements[f"statement{i}"]["clause"] != "and":
-                        ands[j][f"statement{i}"] = self.statements[f"statement{i}"]
-                    ands[j][f"statement{i+1}"] = self.statements[f"statement{i+1}"]
-                    i = i + 1
-                else:
-                    if self.statements[f"statement{i}"]["clause"] == "and":
-                        j = j + 1
-                    i = i + 1
-                self.__link_ands(ands, i, j)
-        except Exception as e:
-            print(e)
+        if i < len(self.statements):
+            if self.statements[f"statement{i+1}"]["clause"] == "and":
+                if self.statements[f"statement{i}"]["clause"] != "and":
+                    ands[j][f"statement{i}"] = self.statements[f"statement{i}"]
+                ands[j][f"statement{i+1}"] = self.statements[f"statement{i+1}"]
+                i = i + 1
+            else:
+                if self.statements[f"statement{i}"]["clause"] == "and":
+                    j = j + 1
+                i = i + 1
+            self.__link_ands(ands, i, j)
 
-    def __resolve_statement(self, statement):
+    def __resolve_statement(self, statement: dict) -> str:
         try:
             option = self.__resolve_option(statement["option"])
             argument = self.__resolve_argument(statement["option"], statement["text"])
@@ -102,11 +111,11 @@ class QueryGenerator:
         except Exception as e:
             print(e)
 
-    def __generate_string(self, statement, option, argument):
+    def __generate_string(self, statement: dict, option: str, argument: str) -> str:
         query = f"{{'{statement['field']}': {{'{option}': {argument}}}}}"
         return query
 
-    def __generate_and_statement(self, statements):
+    def __generate_and_statement(self, statements: dict):
         string = ""
         i = 1
         for statement in statements.values():
@@ -117,7 +126,7 @@ class QueryGenerator:
         query = f'"$and": [{string}]'
         return query
 
-    def __generate_query(self, or_statements, and_statements):
+    def __generate_query(self, or_statements, and_statements) -> str:
         string = ""
         if len(or_statements) > 0:
             for i, statement in enumerate(or_statements):
@@ -140,7 +149,7 @@ class QueryGenerator:
             query = f"{{{string}}}"
         return query
 
-    def __add_returns(self, query_string):
+    def __add_returns(self, query_string: str) -> None:
         query_string = query_string.replace("'", '"')
         self.query = query_string
         self.query_string = query_string
@@ -154,7 +163,7 @@ class QueryGenerator:
             self.query_string_pretty = self.__tidy_up_regex(self.query_string_pretty, value)
         self.query = json.loads(self.query)
 
-    def __deep_search_dict(self, dic, searchkey, occurrences=None):
+    def __deep_search_dict(self, dic: dict, searchkey: str, occurrences: list = None) -> list:
         if occurrences is None:
             occurrences = []
         else:
@@ -170,29 +179,29 @@ class QueryGenerator:
                     occurrences.append(value)
         return occurrences
 
-    def __tidy_up_regex(self, string, text):
+    def __tidy_up_regex(self, string: str, text: str) -> str:
         # replaces the '' around any string
         # meant to remove '' around regex inside of statement
         string = string.replace(f"'{text}'", text)
         string = string.replace(f'"{text}"', text)
         return string
 
-    def __remove_regex(self, string, text):
+    def __remove_regex(self, string: str, text: str) -> str:
         # removes the JS regex indicators // and the option from a string
         string = string.replace(f"{text}", text.replace("/m", "").replace("/", ""))
         string = string.replace(f'"{text}"', text)
         return string
 
-    def __resolve_clause(self, clause):
+    def __resolve_clause(self, clause: str) -> str:
         if clause in ["and", "or"]:
             clause = "$and" if clause == "and" else "$or"
             return clause
 
-    def __resolve_option(self, option):
+    def __resolve_option(self, option: str) -> str:
         option = self.options[option]["name"]
         return option
 
-    def __resolve_argument(self, argument, text):
+    def __resolve_argument(self, argument: str, text: str) -> str:
         argument = self.options[argument]["argument"].format(argument=text)
         # if the argument is not serializable by json, enclose it in quotes
         try:
@@ -201,14 +210,26 @@ class QueryGenerator:
             argument = f"'{argument}'"
         return argument
 
-    def prettify(self, statement):
-        if isinstance(statement, str):
-            statement = statement.replace("'", '"')
-            statement = json.loads(statement)
-        statement = f"{json.dumps(statement, indent=4, sort_keys=False, default=self.__json_serial)}"
-        return statement
+    def prettify(self, statement, sort_keys: bool = False) -> str:
+        """
+        Will indent a string or a dict if it's json serializable\n
+        Will raise Exception if either not a dict or a string or if the statement can't be serialized\n
+        :param statement: either dict or string to indent
+        :param sort_keys: bool if the keys inside the string or dict should be sorted by json
+        :return: indented string
+        """
+        if not isinstance(statement, str) and not isinstance(statement, dict):
+            raise ValueError(f"Expected str or dict, received {type(statement)}")
+        try:
+            if isinstance(statement, str):
+                statement = statement.replace("'", '"')
+                statement = json.loads(statement)
+            statement = f"{json.dumps(statement, indent=4, sort_keys=sort_keys, default=self.__json_serial)}"
+            return statement
+        except Exception:
+            raise
 
-    def __json_serial(self, obj):
+    def __json_serial(self, obj: object) -> str:
         # source: https://stackoverflow.com/questions/11875770/how-to-overcome-datetime-datetime-not-json-serializable
         """JSON serializer for objects not serializable by default json code"""
         if isinstance(obj, (datetime, date)):
